e6fe68a21e517c76e2a43642a228f593d80979c3
Franco Bocanegra <franco.bocanegra@outlook.com>
2026-07-05 20:31:47 -0300

    modificacion de home para veer desde el celular

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,18 @@
 import Carousel from 'react-bootstrap/Carousel';
+import "./Home.css"
 
 
 function Home() {
   return (
 <section className="page">
-{/*       <h1 className="section-title">Cabañas Tres Cerros</h1>
+  <div className='celular'>
+       <h1 className="section-title">Cabañas Tres Cerros</h1>
 
       <p className="section-text">
         Un lugar pensado para descansar, conectar con la naturaleza y disfrutar
         la tranquilidad de las sierras.
       </p>
- */}
+ </div>
       <Carousel>
         <Carousel.Item>
           <img
